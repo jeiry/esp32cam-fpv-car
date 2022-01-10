10ac8ab4505ac58edecd8ee8feccfb47d3412665
jeiry <gjeiry@gmail.com>
2022-01-11 00:41:41 +0800

    Create config.h

---
+++ esp/car/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//wifi setting
+#define ssid "xxx" //wifi名
+#define wifipw "xxx" //wifi密码
+
+//websocket
+#define wusername "xxx" 
+#define wpassword "xxx"
+#define waddr "ws.yoyolife.fun"
+#define wport "1234"
+
+#endif // CONFIG_H   
